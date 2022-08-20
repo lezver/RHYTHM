1cbc270cedd8222624c4b85acb0bdf94759a6169
Vladislav <102545184+lezver@users.noreply.github.com>
2022-08-20 14:33:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict ";
+$(document).ready(function () {
+	$(".castomers__content-slider").slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		adaptiveHeight: true,
+	});
+});
